4f678286116550d6e45c1e97001ea0f02771b007
Mats Op de Beeck <matsopdebeeck@icloud.com>
2023-03-15 09:09:28 +0100

    Day 1

--- src/day1/main.rs
+++ src/day1/main.rs
@@ -1,3 +1,29 @@
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    // Use a buffer to read the input file
+    let mut buffer = String::new();
+    // Open the file
+    let mut file = File::open("src/day1/resources/input.txt").expect("Unable to open file");
+    // Read the file into the buffer
+    file.read_to_string(&mut buffer).expect("Unable to read file");
+
+    let mut max_cals: u32 = 0;
+    let mut current_cals: u32 = 0;
+
+    for line in buffer.lines() {
+        // Convert the line to a number
+        let number = line.parse::<u32>();
+        if number.is_ok() {
+            current_cals += number.unwrap();
+        } else {
+            if current_cals > max_cals {
+                max_cals = current_cals;
+            }
+            current_cals = 0;
+        }
+    }
+
+    println!("Max cals: {}", max_cals);
 }
